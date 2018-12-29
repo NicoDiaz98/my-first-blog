d7cd9c2d3df63648250bd5f7e076e97e2a404ee3
Ale Solda <alejandrosolda at g m a i l d o t c o m>
2018-12-29 17:03:56 -0300

    Desactivo modo debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3w@ho4(jeyo7gwn%8#*xs22pud02q7_)do1ddk0t-1h#dw^%l^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'rescateanimal.pythonanywhere.com']
 
 
 # Application definition
